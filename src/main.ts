846cb51829390aef3b0ee9ee8e232f23a44a8527
Ryan Michaud <ryanwmichaud@gmail.com>
2025-04-10 17:17:28 -0500

    modular sliders for adsr

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,35 @@
 import './style.css'
 
-import { setupCounter } from './counter.ts'
-import {keyToFreq} from './freq.ts'
+import {setupSlider} from './slider.ts'
+import {keyToFreq} from   './freq.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <h1>Typing</h1>
-    <div class="card">
+
       <button id="counter" type="button"></button>
+      
+      <label for="attack"> Attack </label>
+      <input id="attack-slider" 
+        name="attack"
+        type="range"
+        value="0.5"
+        min="0.01"
+        max="0.50"
+        step="0.01"/>
+      <p id="attack-value"> </p>
+
+      <label for="decay"> Decay </label>
+      <input id="decay-slider" 
+        type="range"
+        value="0.01"
+        min="0.01"
+        max="0.50"
+        step="0.01"/>
+      <p id="decay-value"> </p>
+
     </div>
   </div>
 `
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
 
 const AudioContext = window.AudioContext 
 const audioCtx = new AudioContext()
@@ -67,14 +84,12 @@ const handleKeydown = (e: KeyboardEvent)=>{
     gain.gain.linearRampToValueAtTime(1, currTime+attack)
     osc.start(currTime)
   }
-
 }
 const rampNoteOff = (osc: OscillatorNode, gain: GainNode, decay: number)=>{
     gain.gain.setValueAtTime(gain.gain.value, audioCtx.currentTime)
     gain.gain.linearRampToValueAtTime(0.0001, audioCtx.currentTime+decay)
     osc.stop(audioCtx.currentTime+decay)
 }
-
 const handleKeyup = (e: KeyboardEvent)=>{
   const note = activeNoteMap.get(e.key)
   if(note){
@@ -103,3 +118,14 @@ const handleKeyup = (e: KeyboardEvent)=>{
 
 document.addEventListener('keydown', handleKeydown)
 document.addEventListener('keyup', handleKeyup)
+
+const attackSlider = document.getElementById('attack-slider') as HTMLInputElement
+const attackValue = document.getElementById('attack-value') as HTMLParagraphElement
+const decaySlider = document.getElementById('decay-slider') as HTMLInputElement
+const decayValue = document.getElementById('decay-value') as HTMLParagraphElement
+
+setupSlider(attackSlider, attackValue, (value)=>{attack = value})
+setupSlider(decaySlider, decayValue, (value)=>{decay = value})
+
+//setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+//setupAttack(document.querySelector<HTMLInputElement>('#attack')!)
